8efa5cf0e8b89c9644c38db7e92eb309ca321505
louisoduor <louisodhiambo20@gmail.com>
2023-12-21 13:47:10 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
